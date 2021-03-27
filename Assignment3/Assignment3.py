48f5e413cca284bc4a047f2d19d1b17c34d8c22a
Tathya Thaker <tathyathaker05@gmail.com>
2021-03-27 10:54:47 -0500

    mostly code done

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -1,117 +1,3 @@
-# import os
-# import math
-# import numpy as np
-# from collections import Counter
-
-# # make prior probabilities dynamic
-
-# sizett = 463
-# x = os.listdir("assignment3_train\\train")
-# spamwc={}
-# hamwc = {}
-# for i in x:
-# 	y = os.listdir("assignment3_train\\train\\" + i)
-# 	if i=="spam":
-# 		for j in y:		
-# 			f = "assignment3_train\\train\\" + i + "\\" + j
-# 			file=open(f,"r", errors = 'ignore')
-# 			for word in file.read().split():
-# 				if word not in spamwc:
-# 					spamwc[word] = 1
-# 				else:
-# 					spamwc[word] += 1
-# 	else:
-# 		for j in y:		
-# 			f = "assignment3_train\\train\\" + i + "\\" + j
-# 			file=open(f,"r", errors = 'ignore')
-# 			for word in file.read().split():
-# 				if word not in hamwc:
-# 					hamwc[word] = 1
-# 				else:
-# 					hamwc[word] += 1
-
-# totalwc = Counter(hamwc) + Counter(spamwc) 
-# totalw_s = sum(spamwc.values())
-# totalw_h = sum(hamwc.values())
-# novoc = len(totalwc)
-# cs = 0
-# ch = 0
-# cst = 0
-# cht = 0
-# # Naive Bayes
-# for i in x:
-	
-# 	y = os.listdir("assignment3_test\\test\\" + i)
-# 	for j in y:
-# 		test_sh = {}
-# 		f = "assignment3_test\\test\\" + i + "\\" + j
-# 		file=open(f,"r", errors = 'ignore')
-# 		for word in file.read().split():
-# 			if word not in test_sh:
-# 				test_sh[word] = 1
-# 			else:
-# 				test_sh[word] += 1
-# 		prob_s = math.log(123/sizett)
-# 		prob_h = math.log(340/sizett)
-# 		# print(prob_s, prob_h)
-# 		for k in test_sh:
-# 			if spamwc.get(k) != None:
-# 				prob_s = prob_s + math.log((spamwc.get(k)+1)/((totalw_s)+(novoc)))
-# 			else:
-# 				prob_s = prob_s + math.log((1)/((totalw_s)+(novoc)))
-# 			if hamwc.get(k) != None:
-# 				prob_h = prob_h + math.log((hamwc.get(k)+1)/((totalw_h)+(novoc)))
-# 			else:
-# 				prob_h = prob_h + math.log((1)/((totalw_h)+(novoc)))
-
-# 		if prob_s > prob_h:
-# 			cs = cs + 1
-# 			if i=="spam":
-# 				cst = cst + 1
-# 		elif prob_h > prob_s:
-# 			ch = ch + 1
-# 			if i=="ham":
-# 				cht = cht + 1
-
-# print(cst/cs,cht/ch)
-
-
-# # Logistic Regression
-# ltotalwc = list(totalwc.keys())
-# mat = np.zeros((sizett+1,len(ltotalwc)+1))
-# ind = 0
-# calc = 0
-# for i in x:
-# 	y = os.listdir("assignment3_train\\train\\" + i)
-# 	print(ind)
-# 	for j in y:
-# 		logwc = {}
-# 		f = "assignment3_train\\train\\" + i + "\\" + j
-# 		file=open(f,"r", errors = 'ignore')
-# 		for word in file.read().split():
-# 			if word not in logwc:
-# 				logwc[word] = 1
-# 			else:
-# 				logwc[word] += 1
-# 		for k in logwc:
-# 			mat[ind][ltotalwc.index(k)] = logwc[k]
-# 		if i=="spam":
-# 			# print(i)
-# 			calc = calc + 1
-# 			mat[ind][len(ltotalwc)] = 1
-# 		ind = ind + 1
-# print(calc)
-# print(mat[341])
-
-
-
-
-
-
-
-
-
-
 import os
 import math
 import numpy as np
@@ -196,8 +82,6 @@ print(cst/cs,cht/ch,(cst+cht)/(cs+ch))
 
 
 
-
-
 # Logistic regression
 
 
@@ -235,10 +119,51 @@ def prob(w,x):
     return p
 
 
+# 888-676-7698
+w_new = np.ones(len(totalwc)+1)
 w = np.ones(len(totalwc)+1)
-probab = np.ones(mat.shape[0])
-print(mat.shape[0])
-for i in range(mat.shape[0]):
-	probab[i] = prob(w,mat[i])
+# Here the weight calc is done, right now its for one iteration only but code is writtem with w and w_new so i can easily iterate once the code is working
+for i in range(1):
+    temp = 0
+    for j in range(mat.shape[0]):
+        temp = temp + mat[j][i]*((mat[j][mat.shape[1]-1])-prob(w,mat[j]))
+    w_new[i] = w[i]+ (0.01 * temp) - (0.01*0.01*w[i])
 
-print(probab)
+
+# 478 is the total no of files in test folder, the below code creates matrix for test
+mat_test = np.zeros((478,len(ltotalwc)+1))
+ind = 0
+for i in x:
+    y = os.listdir("assignment3_test\\test\\" + i)
+    for j in y:
+        logwc = {}
+        f = "assignment3_test\\test\\" + i + "\\" + j
+        file=open(f,"r", errors = 'ignore')
+        for word in file.read().split():
+            if word not in logwc and word.isalpha():
+                logwc[word] = 1
+            elif word.isalpha():
+                logwc[word] += 1
+        for k in logwc:
+            if k in ltotalwc:
+                mat_test[ind][ltotalwc.index(k)] = logwc[k]
+        if i=="spam":
+            mat_test[ind][len(ltotalwc)] = 1
+        ind = ind + 1
+
+
+
+th = 0
+ts = 0
+tt = 0
+for i in range(mat_test.shape[0]):
+    s = 0
+    for j in range(mat_test.shape[1]-1):
+        s = s + (w_new[j]*mat_test[i][j])
+    s = s + w[0]
+    tt += 1
+    if mat_test[i][len(ltotalwc)]==1 and s>0: #if i change this s>0 to s<0 then the accuracy becomes 94% but I am not sure which one is right
+        ts += 1
+    else:
+        th += 1
+print(ts,th,tt,ts+th/tt)
